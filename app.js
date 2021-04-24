f6e32d00e6ee01defc725fc23581e7760372a13f
teejohn247 <teejohn247@gmail.com>
2021-04-24 17:05:31 +0100

    fixed bugs

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ if (process.env.NODE_ENV === 'production') {
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://devbook-front.herokuapp.com",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
